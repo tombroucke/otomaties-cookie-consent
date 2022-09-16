fff12edd2c7473db8400d7d7ce6a00ea5b85ecf0
Tom Broucke <tom@tombroucke.be>
2022-09-16 16:34:07 +0200

    chore(bump): Bump version to 1.2.1

--- otomaties-cookie-consent.php
+++ otomaties-cookie-consent.php
@@ -5,7 +5,7 @@ namespace Otomaties\CookieConsent;
 /**
  * Plugin Name:       Otomaties cookie consent
  * Description:       Add a cookie consent
- * Version:           1.2.0
+ * Version:           1.2.1
  * Author:            Tom Broucke
  * Author URI:        http://tombroucke.be/
  * License:           GPL-2.0+
