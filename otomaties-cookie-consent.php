36c5e7a429fc60eda4fb5ddc3560560e0fa3fdf3
Tom Broucke <tom@tombroucke.be>
2024-11-18 19:54:46 +0100

    🩹 fix: jit load textdomain

--- otomaties-cookie-consent.php
+++ otomaties-cookie-consent.php
@@ -52,7 +52,7 @@ function init()
         require_once(ABSPATH . 'wp-admin/includes/plugin.php');
     }
 
-    $pluginData = \get_plugin_data(__FILE__);
+    $pluginData = \get_plugin_data(__FILE__, false, false);
     $pluginData['pluginName'] = basename(__FILE__, '.php');
 
     $plugin = new Plugin($pluginData);
